1e468235979b072aa0f91a626f42ed81115b9671
Rt3mis10 <117369494+hrinka@users.noreply.github.com>
2024-11-05 17:46:15 +0900

    github push2

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,8 +1,10 @@
 #include "ScalarConverter.hpp"
 #include <iostream>
 
-int main(int argc, char* argv[]) {
-    if (argc != 2) {
+int main(int argc, char* argv[])
+{
+    if (argc != 2)
+    {
         std::cerr << "Usage: ./convert <literal>" << std::endl;
         return 1;
     }
